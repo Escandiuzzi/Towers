1d31923187c6f62da539cc13f1e4fb670ef77895
Luiz Felipe Escandiuzzi <37418595+Escandiuzzi@users.noreply.github.com>
2018-07-11 19:17:45 -0300

    Initial Commit

---
+++ TowersProject/Assets/Scripts/InputScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputScript : MonoBehaviour {
+    [SerializeField]
+    bool playable;
+
+    public float value;
+    public float gValue;
+	
+    // Use this for initialization
+	void Start () {
+        playable = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (playable)
+        {
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                gameObject.transform.position = gameObject.transform.position + new Vector3(value, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                gameObject.transform.position = gameObject.transform.position + new Vector3(-value, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                gameObject.GetComponent<Rigidbody2D>().gravityScale += gValue;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                transform.Rotate(Vector3.forward * -90);
+            }
+
+
+        }
+
+	}
+
+    public bool GetStatus()
+    {
+        return playable;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var col = collision.gameObject;
+
+        if (col.tag == "Object")
+        {
+            playable = false;
+        }
+    }
+}
---
+++ TowersProject/Assets/Scripts/PieceManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceManager : MonoBehaviour {
+
+    public GameObject[] pieces;
+    public GameObject actualPiece;
+    public float value;
+    public float gValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (actualPiece == null)
+        {
+            var rand = Random.Range(0,7);
+            actualPiece = Instantiate(pieces[rand], transform.position, Quaternion.identity) as GameObject;
+            actualPiece.GetComponent<InputScript>().value = value;
+            actualPiece.GetComponent<InputScript>().gValue = gValue;
+        }
+
+        else if (actualPiece)
+        {
+            if (actualPiece.GetComponent<InputScript>().GetStatus() == false)
+            {
+                actualPiece = null;
+            }
+
+
+        }
+
+  
+
+	}
+}
